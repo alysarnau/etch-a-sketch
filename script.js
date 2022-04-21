442678119ba44287d2981a0c2e33e5c7fa6cc4b9
Alys <alice.g.cooper@gmail.com>
2022-04-21 14:45:27 -0400

    Update JS to dynamically create divs

--- script.js
+++ script.js
@@ -1,52 +1,11 @@
 const container = document.querySelector(".container");
+// create divs
+let n=16; //take grid column value as you want
 
-const div1 = document.createElement('div');
-container.appendChild(div1);
-const div2 = document.createElement('div');
-container.appendChild(div2);
-const div3 = document.createElement('div');
-container.appendChild(div3);
-const div4 = document.createElement('div');
-container.appendChild(div4);
-const div5 = document.createElement('div');
-container.appendChild(div5);
-const div6 = document.createElement('div');
-container.appendChild(div6);
-const div7 = document.createElement('div');
-container.appendChild(div7);
-const div8 = document.createElement('div');
-container.appendChild(div8);
-const div9 = document.createElement('div');
-container.appendChild(div9);
-const div10 = document.createElement('div');
-container.appendChild(div10);
-const div11 = document.createElement('div');
-container.appendChild(div11);
-const div12 = document.createElement('div');
-container.appendChild(div12);
-const div13 = document.createElement('div');
-container.appendChild(div13);
-const div14 = document.createElement('div');
-container.appendChild(div14);
-const div15 = document.createElement('div');
-container.appendChild(div15);
-const div16 = document.createElement('div');
-container.appendChild(div16);
-
-div1.setAttribute('class','div');
-div2.setAttribute('class','div');
-div3.setAttribute('class','div');
-div4.setAttribute('class','div');
-div5.setAttribute('class','div');
-div6.setAttribute('class','div');
-div7.setAttribute('class','div');
-div8.setAttribute('class','div');
-div9.setAttribute('class','div');
-div10.setAttribute('class','div');
-div11.setAttribute('class','div');
-div12.setAttribute('class','div');
-div13.setAttribute('class','div');
-div14.setAttribute('class','div');
-div15.setAttribute('class','div');
-div16.setAttribute('class','div');
-
+for(var i = 0; i < n; i++) {
+    document.body.innerHTML+='<div class="row">';
+    for(j = 0; j < n; j++) {
+        document.body.innerHTML+='<div class="gridsquare">' + (i*5+j+1) + '</div>';
+    }
+    document.body.innerHTML+='</div>';
+}
