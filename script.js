d4139ab7acabf39b86205f7b2cb4186b96cfb33d
Alys <alice.g.cooper@gmail.com>
2022-04-21 14:55:47 -0400

    Set up script to defer, Divs in grid

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ const container = document.querySelector(".container");
 let n=16; //take grid column value as you want
 
 for(var i = 0; i < n; i++) {
-    document.body.innerHTML+='<div class="row">';
+    container.innerHTML+='<div class="row">';
     for(j = 0; j < n; j++) {
-        document.body.innerHTML+='<div class="gridsquare">' + (i*5+j+1) + '</div>';
+        container.innerHTML+='<div class="gridsquare">' + (i*5+j+1) + '</div>';
     }
-    document.body.innerHTML+='</div>';
+    container.innerHTML+='</div>';
 }
+
+const gridsquare = document.querySelectorAll('.gridsquare');
+// gridsquare.textContent("grid");
